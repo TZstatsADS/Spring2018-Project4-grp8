5b41536fe682cf1f8149960fde20699c78078296
xueyao0917 <35582242+xueyao0917@users.noreply.github.com>
2018-04-09 13:56:35 -0400

    preprocess

--- data/README.md
+++ data/README.md
@@ -4,4 +4,8 @@
 
 The data directory contains data used in the analysis. This is treated as read only; in paricular the R/python files are never allowed to write to the files in here. Depending on the project, these might be csv files, a database, and the directory itself may have subdirectories.
 
-Data Set: https://archive.ics.uci.edu/ml/datasets/Anonymous+Microsoft+Web+Data
+Data Set:
+ 
+The first data set is Anonymous Microsoft Web Data (https://archive.ics.uci.edu/ml/datasets/ Anonymous+Microsoft+Web+Data). It?s an example of implicit voting data, with each vroot characterized as being visited (vote of one) or not (no vote).
+
+The second data set is EachMovie (http://www.gatsby.ucl.ac.uk/~chuwei/data/EachMovie/ eachmovie.html). This is an explicit voting example using data, with votes ranging in value from 0 to 5.
---
+++ lib/preprocess.R
@@ -0,0 +1,52 @@
+# ms
+## For each user, there is a case line followed by zero or more vote lines.
+ms_mat <- function(df){
+  case <- df[df$V1=="C",2]
+  vote <- sort(unique(df[df$V1=="V",2]))
+  mat <- matrix(0, nrow=length(case), ncol=length(vote))
+  rownames(mat) <- as.character(case)
+  colnames(mat) <- as.character(vote)
+  df$chunk <- cumsum(df$V1 == "C")
+  for (i in 1:length(case)){
+    caseid <- df$V2[which((df$chunk == i) & (df$V1 == "C"))]
+    voteid <- df$V2[which((df$chunk == i) & (df$V1 == "V"))]
+    for (j in voteid){
+      mat[as.character(caseid), as.character(j)] <- 1
+    }
+  }
+  return(mat)
+}
+
+ms_train <- read.csv("../data/MS_sample/data_train.csv")[,-1]
+ms_test <- read.csv("../data/MS_sample/data_test.csv")[,-1]
+ms_train_mat <- ms_mat(ms_train)
+save(ms_train_mat, file="../output/ms_train_mat.Rdata")
+ms_test_mat <- ms_mat(ms_test)
+save(ms_test_mat, file="../output/ms_test_mat.Rdata")
+
+
+
+# movie
+## zero-to-five star ratings are recorded as integers 1-6
+movie_mat <- function(df){
+  movie <- sort(unique(df$Movie))
+  user <- unique(df$User)
+  mat <- matrix(NA, nrow=length(user), ncol=length(movie))
+  rownames(mat) <- as.character(user)
+  colnames(mat) <- as.character(movie)
+  for (i in user){
+    idx <- which(df$User == i)
+    movies <- df$Movie[idx]
+    scores <- df$Score[idx]
+    movie_idx <- which(movie %in% movies)
+    mat[as.character(i), movie_idx] = scores
+  }
+  return(mat)
+}
+
+movie_train <- read.csv("../data/eachmovie_sample/data_train.csv")[,-1]
+movie_test <- read.csv("../data/eachmovie_sample/data_test.csv")[,-1]
+movie_train_mat <- movie_mat(movie_train)
+save(movie_train_mat, file="../output/movie_train_mat.Rdata")
+movie_test_mat <- movie_mat(movie_test)
+save(movie_test_mat, file="../output/movie_test_mat.Rdata")
